b76fa58a742ede65874890e42a6dc3746e44d89d
lyger <lygerzero0zero@gmail.com>
2023-10-10 19:09:14 +0900

    Greatly simplify and improve group list logic

--- matsuri_monitor/chat/group_list.py
+++ matsuri_monitor/chat/group_list.py
@@ -13,7 +13,7 @@ class GroupList:
         grouper
             Grouper used to define this group list
         """
-        self.groups = []
+        self._groups = []
         self.grouper = grouper
         self.description = grouper.description
         self.notify = grouper.notify
@@ -22,50 +22,36 @@ class GroupList:
 
     def update(self, messages: List[Message]):
         """Compute new groups from the given list of Messages"""
-        new_group_indices = []
-        for i in range(self.last_message_index, len(messages)):
-            message = messages[i]
+        for message in messages[self.last_message_index :]:
             if self.grouper.condition(message):
                 message_interval = message.timestamp - self.last_timestamp
                 if message_interval <= self.grouper.interval:
                     self.add_to_last_group(message)
                 else:
-                    # Delete last group if it's too short
-                    if self.groups and len(self.groups[-1]) < self.grouper.min_len:
-                        self.groups = self.groups[:-1]
-                    new_group_indices.append(i)
                     self.add_to_new_group(message)
 
-        # Delete last group if it's too short
-        if self.groups and len(self.groups[-1]) < self.grouper.min_len:
-            self.groups = self.groups[:-1]
-
-        # Set to the start of the most recent (partial) group
-        if new_group_indices:
-            self.last_message_index = new_group_indices[-1]
-        # If the group interval has already been exceeded, set to end of messages
-        if (
-            messages
-            and (messages[-1].timestamp - messages[self.last_message_index].timestamp)
-            > self.grouper.interval
-        ):
-            self.last_message_index = len(messages) - 1
+        self.last_message_index = len(messages)
 
     def add_to_new_group(self, message: Message):
         """Add message to a new group"""
-        self.groups.append([message])
+        self._groups.append([message])
         self.last_timestamp = message.timestamp
 
     def add_to_last_group(self, message: Message):
         """Add message to current last group"""
-        if len(self.groups) == 0:
+        if len(self._groups) == 0:
             return self.add_to_new_group(message)
         if self.grouper.unique_author and any(
-            message.author == other.author for other in self.groups[-1]
+            message.author == other.author for other in self._groups[-1]
         ):
             return
-        self.groups[-1].append(message)
+        self._groups[-1].append(message)
         self.last_timestamp = message.timestamp
+
+    @property
+    def groups(self) -> List[List[Message]]:
+        """Return filtered groups."""
+        return list(filter(lambda g: len(g) >= self.grouper.min_len, self._groups))
 
     def __len__(self):
         """Number of groups in this list"""
